4185d69ee55209dfa8522a4d9f1f83eaaeb76250
jeremy <jeremy@rocketscientry.com>
2026-04-29 02:23:12 +0000

    refactor: moving all config generation stuff to config_generate.go

--- internal/cli/config_generate.go
+++ internal/cli/config_generate.go
@@ -3,23 +3,229 @@ package cli
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/jjack/remote-boot-agent/internal/bootloader"
 	"github.com/jjack/remote-boot-agent/internal/config"
 	"github.com/jjack/remote-boot-agent/internal/homeassistant"
+	"github.com/jjack/remote-boot-agent/internal/initsystem"
 	"github.com/jjack/remote-boot-agent/internal/system"
 	"github.com/spf13/cobra"
 )
 
 var (
-	discoverHomeAssistant = homeassistant.Discover
-	detectSystemHostname  = system.DetectHostname
-	getSystemInterfaces   = system.GetInterfaceOptions
-	runGenerateSurvey     = GenerateConfigSurvey
-	saveConfigFile        = config.Save
+	surveyAskOne                = survey.AskOne
+	systemGetBroadcastAddresses = system.GetBroadcastAddresses
+	discoverHomeAssistant       = homeassistant.Discover
+	detectSystemHostname        = system.DetectHostname
+	getSystemInterfaces         = system.GetInterfaceOptions
+	saveConfigFile              = config.Save
+	runGenerateSurvey           = generateConfigInteractive
 )
 
-func ensureSupportedSystems(ctx context.Context, deps *CommandDeps) error {
+type haDiscoveryResult struct {
+	url string
+	err error
+}
+
+func surveyValidator(valFunc func(string) error) survey.Validator {
+	return func(val interface{}) error {
+		if str, ok := val.(string); ok {
+			return valFunc(str)
+		}
+		return nil
+	}
+}
+
+func askHostConfig() (config.HostConfig, error) {
+	hostname, err := detectSystemHostname()
+	if err != nil {
+		return config.HostConfig{}, err
+	}
+
+	var finalHostname string
+	err = surveyAskOne(&survey.Input{
+		Message: "Hostname (how Home Assistant will refer to your machine):",
+		Default: hostname,
+	}, &finalHostname, survey.WithValidator(surveyValidator(config.ValidateHostname)))
+	if err != nil {
+		return config.HostConfig{}, err
+	}
+
+	interfaceOptions, err := getSystemInterfaces()
+	if err != nil {
+		return config.HostConfig{}, err
+	}
+
+	var ifaceOptions []string
+	ifaceMap := make(map[string]string)
+	for _, opt := range interfaceOptions {
+		ifaceOptions = append(ifaceOptions, opt.Label)
+		ifaceMap[opt.Label] = opt.Value
+	}
+
+	var selectedIfaceLabel string
+	err = surveyAskOne(&survey.Select{
+		Message: "Select Physical WOL Interface",
+		Options: ifaceOptions,
+	}, &selectedIfaceLabel)
+	if err != nil {
+		return config.HostConfig{}, err
+	}
+
+	macAddress := ifaceMap[selectedIfaceLabel]
+	if err := config.ValidateMACAddress(macAddress); err != nil {
+		return config.HostConfig{}, err
+	}
+
+	broadcastAddrs, _ := systemGetBroadcastAddresses(macAddress)
+	var chosenBroadcast string
+	if len(broadcastAddrs) > 0 {
+		if len(broadcastAddrs) == 1 {
+			chosenBroadcast = broadcastAddrs[0]
+		} else {
+			err = surveyAskOne(&survey.Select{
+				Message: "Multiple WOL Subnet/Broadcast Addresses were discovered. Please select one:",
+				Options: broadcastAddrs,
+			}, &chosenBroadcast)
+			if err != nil {
+				return config.HostConfig{}, err
+			}
+		}
+	}
+
+	var finalBroadcast string
+	err = surveyAskOne(&survey.Input{
+		Message: "WOL Broadcast Address:",
+		Default: chosenBroadcast,
+	}, &finalBroadcast)
+	if err != nil {
+		return config.HostConfig{}, err
+	}
+
+	var wolPortStr string
+	err = surveyAskOne(&survey.Input{
+		Message: "Wake-on-LAN Port (leave blank for default):",
+	}, &wolPortStr, survey.WithValidator(surveyValidator(config.ValidateBroadcastPort)))
+	if err != nil {
+		return config.HostConfig{}, err
+	}
+
+	wolPort, _ := strconv.Atoi(wolPortStr)
+	if wolPort == 0 {
+		wolPort = 9
+	}
+
+	return config.HostConfig{
+		Hostname:         finalHostname,
+		MACAddress:       macAddress,
+		BroadcastAddress: finalBroadcast,
+		BroadcastPort:    wolPort,
+	}, nil
+}
+
+func askBootloaderConfig(ctx context.Context, registry *bootloader.Registry) (config.BootloaderConfig, error) {
+	var blName string
+	err := surveyAskOne(&survey.Select{
+		Message: "Bootloader:",
+		Options: registry.SupportedBootloaders(),
+	}, &blName)
+	if err != nil {
+		return config.BootloaderConfig{}, err
+	}
+
+	var blPath string
+	bl := registry.Get(blName)
+	if bl != nil {
+		blPath, _ = bl.DiscoverConfigPath(ctx)
+	}
+
+	if blPath != "" {
+		err = surveyAskOne(&survey.Input{
+			Message: "Bootloader Config Path:",
+			Default: blPath,
+		}, &blPath)
+		if err != nil {
+			return config.BootloaderConfig{}, err
+		}
+	}
+
+	return config.BootloaderConfig{Name: blName, ConfigPath: blPath}, nil
+}
+
+func askInitSystemConfig(registry *initsystem.Registry) (config.InitSystemConfig, error) {
+	var initSysName string
+	err := surveyAskOne(&survey.Select{
+		Message: "Init System:",
+		Options: registry.SupportedInitSystems(),
+	}, &initSysName)
+	if err != nil {
+		return config.InitSystemConfig{}, err
+	}
+
+	return config.InitSystemConfig{Name: initSysName}, nil
+}
+
+func askHomeAssistantConfig(discoveryChan <-chan haDiscoveryResult) (config.HomeAssistantConfig, error) {
+	discoveryResult := <-discoveryChan
+	var finalHassURL string
+	err := surveyAskOne(&survey.Input{
+		Message: "Home Assistant URL:",
+		Default: discoveryResult.url,
+	}, &finalHassURL, survey.WithValidator(surveyValidator(config.ValidateURL)))
+	if err != nil {
+		return config.HomeAssistantConfig{}, err
+	}
+
+	var webhookID string
+	err = surveyAskOne(&survey.Input{
+		Message: "Home Assistant Webhook ID:",
+	}, &webhookID, survey.WithValidator(surveyValidator(config.ValidateWebhookID)))
+	if err != nil {
+		return config.HomeAssistantConfig{}, err
+	}
+
+	return config.HomeAssistantConfig{URL: finalHassURL, WebhookID: webhookID}, nil
+}
+
+func generateConfigInteractive(ctx context.Context, deps *CommandDeps) (*config.Config, error) {
+	haDiscoveryResultChan := make(chan haDiscoveryResult, 1)
+	go func() {
+		url, err := discoverHomeAssistant()
+		haDiscoveryResultChan <- haDiscoveryResult{url: url, err: err}
+	}()
+
+	hostCfg, err := askHostConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	blCfg, err := askBootloaderConfig(ctx, deps.BootloaderRegistry)
+	if err != nil {
+		return nil, err
+	}
+
+	initCfg, err := askInitSystemConfig(deps.InitRegistry)
+	if err != nil {
+		return nil, err
+	}
+
+	haCfg, err := askHomeAssistantConfig(haDiscoveryResultChan)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config.Config{
+		Host:          hostCfg,
+		Bootloader:    blCfg,
+		InitSystem:    initCfg,
+		HomeAssistant: haCfg,
+	}, nil
+}
+
+func ensureSupport(ctx context.Context, deps *CommandDeps) error {
 	_, err := deps.BootloaderRegistry.Detect(ctx)
 	if err != nil {
 		if err.Error() == "no supported bootloader detected" {
@@ -46,19 +252,11 @@ func NewConfigGenerateCmd(deps *CommandDeps) *cobra.Command {
 		Use:   "generate",
 		Short: "Interactively generate a config file",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := ensureSupportedSystems(cmd.Context(), deps); err != nil {
+			if err := ensureSupport(cmd.Context(), deps); err != nil {
 				return err
 			}
 
-			opts := GenerateSurveyOptions{
-				DiscoverHomeAssistant: discoverHomeAssistant,
-				DetectHostname:        detectSystemHostname,
-				GetInterfaces:         getSystemInterfaces,
-				BootloaderOptions:     deps.BootloaderRegistry.SupportedBootloaders(),
-				InitSystemOptions:     deps.InitRegistry.SupportedInitSystems(),
-			}
-
-			cfg, err := runGenerateSurvey(opts)
+			cfg, err := runGenerateSurvey(cmd.Context(), deps)
 			if err != nil {
 				return err
 			}
--- internal/cli/config_generate_test.go
+++ internal/cli/config_generate_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/jjack/remote-boot-agent/internal/bootloader"
 	"github.com/jjack/remote-boot-agent/internal/config"
 	"github.com/jjack/remote-boot-agent/internal/initsystem"
@@ -52,16 +53,10 @@ func (m *mockInactiveBootloader) DiscoverConfigPath(ctx context.Context) (string
 }
 
 func TestGenerateConfigCmd_Execute(t *testing.T) {
-	oldDiscover := discoverHomeAssistant
-	oldDetectHostname := detectSystemHostname
-	oldGetInterfaces := getSystemInterfaces
 	oldRunForm := runGenerateSurvey
 	oldSave := saveConfigFile
 
 	defer func() {
-		discoverHomeAssistant = oldDiscover
-		detectSystemHostname = oldDetectHostname
-		getSystemInterfaces = oldGetInterfaces
 		runGenerateSurvey = oldRunForm
 		saveConfigFile = oldSave
 	}()
@@ -75,18 +70,7 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 		{
 			name: "Happy Path",
 			setupMocks: func(deps *CommandDeps) {
-				discoverHomeAssistant = func() (string, error) { return "http://hass.local", nil }
-				detectSystemHostname = func() (string, error) { return "test-host", nil }
-				getSystemInterfaces = func() ([]system.InterfaceInfo, error) {
-					return []system.InterfaceInfo{{Label: "eth0", Value: "00:11:22:33:44:55"}}, nil
-				}
-				runGenerateSurvey = func(opts GenerateSurveyOptions) (*config.Config, error) {
-					if _, err := opts.DetectHostname(); err != nil {
-						return nil, err
-					}
-					if _, err := opts.GetInterfaces(); err != nil {
-						return nil, err
-					}
+				runGenerateSurvey = func(ctx context.Context, deps *CommandDeps) (*config.Config, error) {
 					return &config.Config{}, nil
 				}
 				saveConfigFile = func(cfg *config.Config, path string) error { return nil }
@@ -96,12 +80,8 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 		{
 			name: "Hostname Error",
 			setupMocks: func(deps *CommandDeps) {
-				detectSystemHostname = func() (string, error) { return "", errors.New("hostname fail") }
-				runGenerateSurvey = func(opts GenerateSurveyOptions) (*config.Config, error) {
-					if _, err := opts.DetectHostname(); err != nil {
-						return nil, err
-					}
-					return &config.Config{}, nil
+				runGenerateSurvey = func(ctx context.Context, deps *CommandDeps) (*config.Config, error) {
+					return nil, errors.New("hostname fail")
 				}
 			},
 			wantErr:     true,
@@ -110,16 +90,8 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 		{
 			name: "Interfaces Error",
 			setupMocks: func(deps *CommandDeps) {
-				detectSystemHostname = func() (string, error) { return "test-host", nil }
-				getSystemInterfaces = func() ([]system.InterfaceInfo, error) { return nil, errors.New("iface fail") }
-				runGenerateSurvey = func(opts GenerateSurveyOptions) (*config.Config, error) {
-					if _, err := opts.DetectHostname(); err != nil {
-						return nil, err
-					}
-					if _, err := opts.GetInterfaces(); err != nil {
-						return nil, err
-					}
-					return &config.Config{}, nil
+				runGenerateSurvey = func(ctx context.Context, deps *CommandDeps) (*config.Config, error) {
+					return nil, errors.New("iface fail")
 				}
 			},
 			wantErr:     true,
@@ -148,9 +120,7 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 		{
 			name: "Form Error",
 			setupMocks: func(deps *CommandDeps) {
-				detectSystemHostname = func() (string, error) { return "test-host", nil }
-				getSystemInterfaces = func() ([]system.InterfaceInfo, error) { return []system.InterfaceInfo{}, nil }
-				runGenerateSurvey = func(opts GenerateSurveyOptions) (*config.Config, error) {
+				runGenerateSurvey = func(ctx context.Context, deps *CommandDeps) (*config.Config, error) {
 					return nil, errors.New("form canceled")
 				}
 			},
@@ -160,12 +130,7 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 		{
 			name: "DiscoverConfigPath Fails But Proceeds",
 			setupMocks: func(deps *CommandDeps) {
-				discoverHomeAssistant = func() (string, error) { return "http://hass.local", nil }
-				detectSystemHostname = func() (string, error) { return "test-host", nil }
-				getSystemInterfaces = func() ([]system.InterfaceInfo, error) {
-					return []system.InterfaceInfo{{Label: "eth0", Value: "00:11:22:33:44:55"}}, nil
-				}
-				runGenerateSurvey = func(opts GenerateSurveyOptions) (*config.Config, error) {
+				runGenerateSurvey = func(ctx context.Context, deps *CommandDeps) (*config.Config, error) {
 					return &config.Config{}, nil
 				}
 				saveConfigFile = func(cfg *config.Config, path string) error { return nil }
@@ -179,9 +144,7 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 		{
 			name: "Save Config Error",
 			setupMocks: func(deps *CommandDeps) {
-				detectSystemHostname = func() (string, error) { return "test-host", nil }
-				getSystemInterfaces = func() ([]system.InterfaceInfo, error) { return []system.InterfaceInfo{}, nil }
-				runGenerateSurvey = func(opts GenerateSurveyOptions) (*config.Config, error) { return &config.Config{}, nil }
+				runGenerateSurvey = func(ctx context.Context, deps *CommandDeps) (*config.Config, error) { return &config.Config{}, nil }
 				saveConfigFile = func(cfg *config.Config, path string) error { return errors.New("save fail") }
 			},
 			wantErr:     true,
@@ -214,4 +177,230 @@ func TestGenerateConfigCmd_Execute(t *testing.T) {
 			}
 		})
 	}
+}
+
+type mockSurveyBootloader struct{}
+
+func (m *mockSurveyBootloader) Name() string                      { return "grub" }
+func (m *mockSurveyBootloader) IsActive(ctx context.Context) bool { return true }
+func (m *mockSurveyBootloader) GetBootOptions(ctx context.Context, cfg bootloader.Config) ([]string, error) {
+	return nil, nil
+}
+
+func (m *mockSurveyBootloader) Install(ctx context.Context, macAddress, haURL string) error {
+	return nil
+}
+
+func (m *mockSurveyBootloader) DiscoverConfigPath(ctx context.Context) (string, error) {
+	return "/boot/grub/grub.cfg", nil
+}
+
+type mockSurveyInitSystem struct{}
+
+func (m *mockSurveyInitSystem) Name() string                                         { return "systemd" }
+func (m *mockSurveyInitSystem) IsActive(ctx context.Context) bool                    { return true }
+func (m *mockSurveyInitSystem) Install(ctx context.Context, configPath string) error { return nil }
+
+func setupSurveyDeps() *CommandDeps {
+	blReg := bootloader.NewRegistry()
+	blReg.Register("grub", func() bootloader.Bootloader { return &mockSurveyBootloader{} })
+
+	initReg := initsystem.NewRegistry()
+	initReg.Register("systemd", func() initsystem.InitSystem { return &mockSurveyInitSystem{} })
+
+	return &CommandDeps{BootloaderRegistry: blReg, InitRegistry: initReg}
+}
+
+func TestSurveyValidator(t *testing.T) {
+	valFunc := func(v string) error {
+		if v == "fail" {
+			return errors.New("validation failed")
+		}
+		return nil
+	}
+
+	validator := surveyValidator(valFunc)
+
+	if err := validator("fail"); err == nil || err.Error() != "validation failed" {
+		t.Errorf("expected validation to fail, got %v", err)
+	}
+	if err := validator("success"); err != nil {
+		t.Errorf("expected validation to succeed, got %v", err)
+	}
+	if err := validator(123); err != nil {
+		t.Errorf("expected non-string to return nil, got %v", err)
+	}
+}
+
+func buildMockSurveyAskOne(triggerErrorOn string) func(survey.Prompt, interface{}, ...survey.AskOpt) error {
+	return func(p survey.Prompt, response interface{}, opts ...survey.AskOpt) error {
+		var msg string
+		switch pt := p.(type) {
+		case *survey.Input:
+			msg = pt.Message
+		case *survey.Select:
+			msg = pt.Message
+		}
+
+		if triggerErrorOn != "" && msg == triggerErrorOn {
+			return errors.New("simulated survey error")
+		}
+
+		switch pt := p.(type) {
+		case *survey.Input:
+			switch pt.Message {
+			case "Hostname (how Home Assistant will refer to your machine):":
+				*(response.(*string)) = "my-host"
+			case "WOL Broadcast Address:":
+				*(response.(*string)) = "192.168.1.255"
+			case "Wake-on-LAN Port (leave blank for default):":
+				*(response.(*string)) = "" // test fallback
+			case "Bootloader Config Path:":
+				*(response.(*string)) = "/boot/grub/grub.cfg"
+			case "Home Assistant URL:":
+				*(response.(*string)) = "http://hass.local:8123"
+			case "Home Assistant Webhook ID:":
+				*(response.(*string)) = "webhook123"
+			}
+		case *survey.Select:
+			switch pt.Message {
+			case "Select Physical WOL Interface":
+				*(response.(*string)) = "eth0"
+			case "Multiple WOL Subnet/Broadcast Addresses were discovered. Please select one:":
+				*(response.(*string)) = "192.168.1.255"
+			case "Bootloader:":
+				*(response.(*string)) = "grub"
+			case "Init System:":
+				*(response.(*string)) = "systemd"
+			}
+		}
+		return nil
+	}
+}
+
+func TestGenerateConfigSurvey_Success(t *testing.T) {
+	oldSurveyAskOne := surveyAskOne
+	oldSystemGetBroadcastAddresses := systemGetBroadcastAddresses
+	oldDiscoverHomeAssistant := discoverHomeAssistant
+	oldDetectSystemHostname := detectSystemHostname
+	oldGetSystemInterfaces := getSystemInterfaces
+	defer func() {
+		surveyAskOne = oldSurveyAskOne
+		systemGetBroadcastAddresses = oldSystemGetBroadcastAddresses
+		discoverHomeAssistant = oldDiscoverHomeAssistant
+		detectSystemHostname = oldDetectSystemHostname
+		getSystemInterfaces = oldGetSystemInterfaces
+	}()
+
+	surveyAskOne = buildMockSurveyAskOne("")
+
+	systemGetBroadcastAddresses = func(mac string) ([]string, error) {
+		return []string{"192.168.1.255", "10.0.0.255"}, nil // Trigger multiple broadcasts path
+	}
+
+	discoverHomeAssistant = func() (string, error) { return "http://hass.local:8123", nil }
+	detectSystemHostname = func() (string, error) { return "detected-host", nil }
+	getSystemInterfaces = func() ([]system.InterfaceInfo, error) {
+		return []system.InterfaceInfo{
+			{Label: "eth0", Value: "00:11:22:33:44:55"},
+		}, nil
+	}
+
+	deps := setupSurveyDeps()
+	cfg, err := generateConfigInteractive(context.Background(), deps)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if cfg.Host.Hostname != "my-host" {
+		t.Errorf("expected hostname my-host, got %s", cfg.Host.Hostname)
+	}
+	if cfg.Host.BroadcastAddress != "192.168.1.255" {
+		t.Errorf("expected BroadcastAddress 192.168.1.255, got %s", cfg.Host.BroadcastAddress)
+	}
+	if cfg.Host.BroadcastPort != 9 {
+		t.Errorf("expected BroadcastPort 9 (fallback), got %d", cfg.Host.BroadcastPort)
+	}
+	if cfg.HomeAssistant.URL != "http://hass.local:8123" {
+		t.Errorf("expected URL http://hass.local:8123, got %s", cfg.HomeAssistant.URL)
+	}
+}
+
+func TestGenerateConfigSurvey_AskOneErrors(t *testing.T) {
+	oldSurveyAskOne := surveyAskOne
+	oldSystemGetBroadcastAddresses := systemGetBroadcastAddresses
+	oldDiscoverHomeAssistant := discoverHomeAssistant
+	oldDetectSystemHostname := detectSystemHostname
+	oldGetSystemInterfaces := getSystemInterfaces
+	defer func() {
+		surveyAskOne = oldSurveyAskOne
+		systemGetBroadcastAddresses = oldSystemGetBroadcastAddresses
+		discoverHomeAssistant = oldDiscoverHomeAssistant
+		detectSystemHostname = oldDetectSystemHostname
+		getSystemInterfaces = oldGetSystemInterfaces
+	}()
+
+	systemGetBroadcastAddresses = func(mac string) ([]string, error) { return []string{"192.168.1.255"}, nil }
+	discoverHomeAssistant = func() (string, error) { return "http://hass.local:8123", nil }
+	detectSystemHostname = func() (string, error) { return "detected-host", nil }
+	getSystemInterfaces = func() ([]system.InterfaceInfo, error) {
+		return []system.InterfaceInfo{{Label: "eth0", Value: "00:11:22:33:44:55"}}, nil
+	}
+
+	deps := setupSurveyDeps()
+	errorSteps := []string{
+		"Hostname (how Home Assistant will refer to your machine):",
+		"Select Physical WOL Interface",
+		"WOL Broadcast Address:",
+		"Wake-on-LAN Port (leave blank for default):",
+		"Bootloader:",
+		"Bootloader Config Path:",
+		"Init System:",
+		"Home Assistant URL:",
+		"Home Assistant Webhook ID:",
+	}
+
+	for _, step := range errorSteps {
+		t.Run("Error at "+step, func(t *testing.T) {
+			surveyAskOne = buildMockSurveyAskOne(step)
+			_, err := generateConfigInteractive(context.Background(), deps)
+			if err == nil || err.Error() != "simulated survey error" {
+				t.Fatalf("expected simulated survey error at step %q, got %v", step, err)
+			}
+		})
+	}
+
+	t.Run("Multiple Subnet Selection Error", func(t *testing.T) {
+		surveyAskOne = buildMockSurveyAskOne("Multiple WOL Subnet/Broadcast Addresses were discovered. Please select one:")
+		systemGetBroadcastAddresses = func(mac string) ([]string, error) { return []string{"192.168.1.255", "10.0.0.255"}, nil }
+		_, err := generateConfigInteractive(context.Background(), deps)
+		if err == nil || err.Error() != "simulated survey error" {
+			t.Errorf("expected simulated survey error, got %v", err)
+		}
+	})
+}
+
+func TestGenerateConfigSurvey_OptErrors(t *testing.T) {
+	t.Run("Invalid MAC Address", func(t *testing.T) {
+		oldSurveyAskOne := surveyAskOne
+		oldDetectSystemHostname := detectSystemHostname
+		oldGetSystemInterfaces := getSystemInterfaces
+		surveyAskOne = buildMockSurveyAskOne("")
+		defer func() {
+			surveyAskOne = oldSurveyAskOne
+			detectSystemHostname = oldDetectSystemHostname
+			getSystemInterfaces = oldGetSystemInterfaces
+		}()
+
+		detectSystemHostname = func() (string, error) { return "host", nil }
+		getSystemInterfaces = func() ([]system.InterfaceInfo, error) {
+			return []system.InterfaceInfo{{Label: "eth0", Value: "invalid-mac"}}, nil
+		}
+
+		deps := setupSurveyDeps()
+		_, err := generateConfigInteractive(context.Background(), deps)
+		if err == nil {
+			t.Errorf("expected mac validation error, got nil")
+		}
+	})
 }
